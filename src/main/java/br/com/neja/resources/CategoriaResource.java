0bbc0714441d2faa9e6ea27f19aba4d2895ceb60
Junior Erdmann Streichan <juniorstreichan@gmail.com>
2018-04-06 11:09:26 -0400

    Listando Categorias com padrao DTO

--- src/main/java/br/com/neja/resources/CategoriaResource.java
+++ src/main/java/br/com/neja/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package br.com.neja.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.neja.domain.Categoria;
+import br.com.neja.dto.CategoriaDTO;
 import br.com.neja.services.CategoriaService;
 
 @RestController
@@ -22,9 +25,18 @@ public class CategoriaResource {
 	private CategoriaService service;
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(
+				obj -> new CategoriaDTO(obj)
+				).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
@@ -40,27 +52,11 @@ public class CategoriaResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
-	
-	@RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-	
-	
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/main/java/br/com/neja/services/CategoriaService.java
+++ src/main/java/br/com/neja/services/CategoriaService.java
@@ -1,7 +1,6 @@
 package br.com.neja.services;
 
-
-
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,51 +17,34 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 
-	public Categoria find(Integer id)     {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElseThrow(
-				() -> new ObjectNotFoundException("Objeto não encontrado! Id:"+id+" Tipo: "+Categoria.class.getSimpleName())
-		);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id:" + id + " Tipo: " + Categoria.class.getSimpleName()));
 	}
-	
-	
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-
 
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
 	}
 
-
 	public void delete(Integer id) {
 		Categoria c = find(id);
 		try {
 			repo.deleteById(c.getId());
 		} catch (DataIntegrityViolationException ex) {
-			throw new DataIntegrirtyException("Não é possível excluir a categoria"+c.getNome()+" pois ela possui produtos");
+			throw new DataIntegrirtyException(
+					"Não é possível excluir a categoria" + c.getNome() + " pois ela possui produtos");
 		}
-		
+
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
