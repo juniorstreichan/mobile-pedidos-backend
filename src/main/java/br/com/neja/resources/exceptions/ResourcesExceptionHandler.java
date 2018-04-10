59bfc3251e734f1d22c84bd70cd3d78b314434a3
Junior Erdmann Streichan <juniorstreichan@gmail.com>
2018-04-10 08:17:42 -0400

    Erros personalizados / BeanValidation

--- src/main/java/br/com/neja/resources/exceptions/ResourcesExceptionHandler.java
+++ src/main/java/br/com/neja/resources/exceptions/ResourcesExceptionHandler.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -29,6 +31,19 @@ public class ResourcesExceptionHandler {
 
 		StandardError erro = new StandardError(HttpStatus.BAD_REQUEST.value(), ex.getMessage(),
 				System.currentTimeMillis());
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
+	}
+	
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException ex, HttpServletRequest req) {
+
+		ValidationError erro = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de Validação",
+				System.currentTimeMillis());
+		for(FieldError x : ex.getBindingResult().getFieldErrors()) {
+			erro.addErrors(x.getField(), x.getDefaultMessage());
+		}
 
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
 	}
---
+++ src/main/java/br/com/neja/resources/exceptions/ValidationError.java
@@ -0,0 +1,27 @@
+package br.com.neja.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+
+	public ValidationError(Integer status, String message, Long timestamp) {
+		super(status, message, timestamp);
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addErrors(String fieldName,String message) {
+		this.errors.add(new FieldMessage(fieldName, message));
+	}
+	
+	
+	
+
+}
