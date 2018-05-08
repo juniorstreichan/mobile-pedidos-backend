0492f21a512ce0f0e2196f12e0a3bfc748b7a59d
Junior Erdmann Streichan <juniorstreichan@gmail.com>
2018-05-07 23:43:22 -0400

    Tratamento de exceções da Amazon

--- src/main/java/br/com/neja/resources/exceptions/ResourcesExceptionHandler.java
+++ src/main/java/br/com/neja/resources/exceptions/ResourcesExceptionHandler.java
@@ -9,14 +9,18 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+
 import br.com.neja.services.exception.AuthorizationException;
 import br.com.neja.services.exception.DataIntegrityException;
+import br.com.neja.services.exception.FileException;
 import br.com.neja.services.exception.ObjectNotFoundException;
 
 @ControllerAdvice
 public class ResourcesExceptionHandler {
 
-	
 	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException ex, HttpServletRequest req) {
 
@@ -25,8 +29,7 @@ public class ResourcesExceptionHandler {
 
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
 	}
-	
-	
+
 	@ExceptionHandler(DataIntegrityException.class)
 	public ResponseEntity<StandardError> DataIntegrity(DataIntegrityException ex, HttpServletRequest req) {
 
@@ -35,20 +38,19 @@ public class ResourcesExceptionHandler {
 
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
 	}
-	
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException ex, HttpServletRequest req) {
 
 		ValidationError erro = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de Validação",
 				System.currentTimeMillis());
-		for(FieldError x : ex.getBindingResult().getFieldErrors()) {
+		for (FieldError x : ex.getBindingResult().getFieldErrors()) {
 			erro.addErrors(x.getField(), x.getDefaultMessage());
 		}
 
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
 	}
-	
+
 	@ExceptionHandler(AuthorizationException.class)
 	public ResponseEntity<StandardError> authorization(AuthorizationException ex, HttpServletRequest req) {
 
@@ -57,4 +59,63 @@ public class ResourcesExceptionHandler {
 
 		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(erro);
 	}
+
+	@ExceptionHandler(FileException.class)
+	public ResponseEntity<StandardError> file(FileException ex, HttpServletRequest req) {
+
+		StandardError erro = new StandardError(HttpStatus.BAD_REQUEST.value(), ex.getMessage(),
+				System.currentTimeMillis());
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
+	}
+	
+	@ExceptionHandler(AmazonS3Exception.class)
+	public ResponseEntity<StandardError> amazonS3(AmazonS3Exception ex, HttpServletRequest req) {
+
+		StandardError erro = new StandardError(HttpStatus.BAD_REQUEST.value(), ex.getMessage(),
+				System.currentTimeMillis());
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
+	}
+	
+	@ExceptionHandler(AmazonClientException.class)
+	public ResponseEntity<StandardError> amazonClient(AmazonClientException ex, HttpServletRequest req) {
+
+		StandardError erro = new StandardError(HttpStatus.BAD_REQUEST.value(), ex.getMessage(),
+				System.currentTimeMillis());
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
+	}
+	
+	@ExceptionHandler(AmazonServiceException.class)
+	public ResponseEntity<StandardError> amazonService(AmazonServiceException ex, HttpServletRequest req) {
+
+		HttpStatus code = HttpStatus.valueOf(ex.getErrorCode());
+		StandardError erro = new StandardError( code.value(), ex.getMessage(),
+				System.currentTimeMillis());
+
+		return ResponseEntity.status(code).body(erro);
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/br/com/neja/services/S3Service.java
+++ src/main/java/br/com/neja/services/S3Service.java
@@ -15,6 +15,8 @@ import org.springframework.web.multipart.MultipartFile;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 
+import br.com.neja.services.exception.FileException;
+
 @Service
 public class S3Service {
 
@@ -37,7 +39,7 @@ public class S3Service {
 			return uploadFile(input, fileName, contentType);
 
 		} catch (IOException e) {
-			throw new RuntimeException("Erro IO: " + e.getMessage());
+			throw new FileException("Erro IO: " + e.getMessage(),e);
 		}
 	}
 
